ae55d118340e40b890625bceaddb67b8a030b4c6
Ngọc Hoàng Minh Hồ <hongochoangm@gmail.com>
2021-12-27 22:00:33 +0700

    add userModel userRoutes and userController

---
+++ controllers/authController.js
@@ -0,0 +1,43 @@
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const createToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+};
+
+const sendToken = (user, statusCode, req, res) => {
+    const token = createToken(user._id);
+    console.log('token: ', token);
+
+    res.cookie('jwt', token, {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+        secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+    });
+    user.password = undefined;
+    res.status(statusCode).json({
+        status:'success',
+        token,
+        data:{
+            user,
+        },
+    });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+    const newUser = await User.create({
+        name = req.body.name,
+        email = req.body.email,
+        password = req.body.password,
+        passwordConfirm = req.body.passwordConfirm,
+        avatar = req.body.avatar,
+    });
+    sendToken(201, newUser, req, res);
+});
