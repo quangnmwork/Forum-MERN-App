1522d6a7836782228e1b027186807fb39a1da154
Ngọc Hoàng Minh Hồ <hongochoangm@gmail.com>
2021-12-28 15:27:44 +0700

    fail

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -25,28 +24,6 @@ app.post(
   app.use(express.json({ limit: '10kb' }));
   app.use(express.urlencoded({ extended: true, limit: '10kb' }));
   app.use(cookieParser());
-=======
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const userRouter = require("./routes/userRoutes");
-const AppError = require("./utils/appError");
-const cookieParser = require("cookie-parser");
-const app = express();
-
-app.use(cors());
-app.options("*", cors());
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
-// body parser
-
-app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({ extended: true, limit: "10kb" }));
-app.use(cookieParser());
-
-// end body parser
->>>>>>> origin/quang
 
 //ROUTE
 app.use("/api/v1/users", userRouter);
