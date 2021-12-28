16e0fb536aeb601acc671b64703c49cc3a8ca311
Nguyen Minh Quang <quangnm.work@gmail.com>
2021-12-28 23:53:44 +0700

    update forgot-password reset-password update-password

--- models/userModel.js
+++ models/userModel.js
@@ -65,9 +65,10 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
   next();
 });
 
@@ -76,20 +77,30 @@ userSchema.pre(/^find/, function (next) {
   next();
 });
 
-userSchema.methods.correctPassword =
-  async function (inputPassword, password) {
-    return await bcrypt.compare(inputPassword, password);
-  };
+userSchema.methods.correctPassword = async function (inputPassword, password) {
+  return await bcrypt.compare(inputPassword, password);
+};
+
 userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   if (this.passwordChangedAt) {
-    const changedTimestamp = parseInt(
-      this.passwordChangedAt.getTime() / 1000,
-      10
-    );
+    const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
 
     return JWTTimestamp < changedTimestamp;
   }
   return false;
 };
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  this.passwordResetToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+
 const User = mongoose.model("User", userSchema);
 module.exports = User;
